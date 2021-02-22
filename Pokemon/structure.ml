00fc4507994e27538cea35916af1a3829f46f8f9
Mickael Laurent <esh4rk@gmail.com>
2021-02-22 20:56:12 +0100

    refactor structure.ml

--- Pokemon/structure.ml
+++ Pokemon/structure.ml
@@ -13,6 +13,13 @@ let int32_to_low_high i =
 
 let tid_from_vid_sid = int32_from_low_high
 
+let data_offset = 32
+let substructure_size = 12
+let data_size = substructure_size * 4
+let pid_offset = 0
+let otid_offset = 4
+let checksum_offset = 28
+
 let decryption_key { pid ; otid ; _ } = logxor pid otid
 
 let substructures_order = [|
@@ -22,6 +29,11 @@ let substructures_order = [|
   "MGAE" ; "MGEA" ; "MAGE" ; "MAEG" ; "MEGA" ; "MEAG" ;
 |]
 
+let pkmn_from_bytes buf =
+  let pid = Bytes.get_int32_le buf pid_offset in
+  let otid = Bytes.get_int32_le buf otid_offset in
+  { pid ; otid }
+
 let substructure_position { pid ; _ } ss =
   let i = unsigned_rem pid (of_int 24) |> Name.int32_to_int in
   let order = substructures_order.(i) in
@@ -30,21 +42,75 @@ let substructure_position { pid ; _ } ss =
   in
   aux 0
 
-let data_offset = 32
-let substructure_size = 12
-
 let substructure_offset pkmn ss =
   let p = substructure_position pkmn ss in
   data_offset + substructure_size*p
 
-let ivea_offset pkmn =
-  (substructure_offset pkmn 'M') + 4
+(* Local operations *)
 
 let decrypt_aligned_int32 pkmn i =
   let key = decryption_key pkmn in
   logxor i key
 
 let encrypt_aligned_int32 = decrypt_aligned_int32
+
+let checksum_diff_for_aligned_int32 o n =
+  let (lo, ho) = int32_to_low_high o in
+  let (ln, hn) = int32_to_low_high n in
+  let diff = add (sub ln lo) (sub hn ho) in
+  logand diff mask16
+
+(* Global operations *)
+
+let enc_dec pkmn buf start len =
+  let rec enc_dec_from offset = 
+    if offset >= start + len then ()
+    else (
+      let i = Bytes.get_int32_le buf offset in
+      let i = encrypt_aligned_int32 pkmn i in
+      Bytes.set_int32_le buf offset i ;
+      enc_dec_from (offset + 4)
+    )
+  in
+  enc_dec_from start
+
+let extract_data buf =
+  let pkmn = pkmn_from_bytes buf in
+  let res = Bytes.sub buf data_offset data_size in
+  enc_dec pkmn res 0 data_size ;
+  res
+
+let compute_checksum buf start len =
+  let rec aux acc i =
+    if i >= start+len
+    then acc
+    else
+      let nb = Bytes.get_uint16_le buf i |> Int32.of_int in
+      aux (Int32.add acc nb) (i+2)
+  in
+  let sum = aux Int32.zero start in
+  let low = Int32.logand sum mask16 in
+  low |> Name.int32_to_int
+
+let update_with_data buf data =
+  let pkmn = pkmn_from_bytes buf in
+  let checksum = compute_checksum data 0 data_size in
+  Bytes.blit data 0 buf data_offset data_size ;
+  enc_dec pkmn buf data_offset data_size ;
+  Bytes.set_uint16_le buf checksum_offset checksum
+
+(* Species *)
+
+let species_offset pkmn =
+  (substructure_offset pkmn 'G')
+
+let species_offset_relative_to_data pkmn =
+  (substructure_offset pkmn 'G') - data_offset
+
+(* IVs *)
+
+let ivea_offset pkmn =
+  (substructure_offset pkmn 'M') + 4
 
 let mask5 = 0b11111 |> of_int
 
@@ -77,9 +143,3 @@ let ivs_to_ivea_data (hp,atk,def,speed,sp_atk,sp_def,data) =
   let data = shift_left data 5 in
   let data = logor data hp in
   data
-
-let checksum_diff_for_aligned_int32 o n =
-  let (lo, ho) = int32_to_low_high o in
-  let (ln, hn) = int32_to_low_high n in
-  let diff = add (sub ln lo) (sub hn ho) in
-  logand diff mask16
--- Pokemon/structure.mli
+++ Pokemon/structure.mli
@@ -5,10 +5,30 @@ val int32_from_low_high : int32 -> int32 -> int32
 val int32_to_low_high : int32 -> int32 * int32
 val tid_from_vid_sid : int32 -> int32 -> int32
 
+val pkmn_from_bytes : Bytes.t -> pokemon
+
 val substructure_position : pokemon -> char -> int
-val ivea_offset : pokemon -> int
+
+(* Global operations *)
+
+val extract_data : Bytes.t -> Bytes.t
+val update_with_data : Bytes.t -> Bytes.t -> unit
+
+(* Local operations *)
+
 val decrypt_aligned_int32 : pokemon -> int32 -> int32
 val encrypt_aligned_int32 : pokemon -> int32 -> int32
+
+val checksum_diff_for_aligned_int32 : int32 -> int32 -> int32
+
+(* Species *)
+
+val species_offset : pokemon -> int
+val species_offset_relative_to_data : pokemon -> int
+
+(* IVs *)
+
+val ivea_offset : pokemon -> int
 
 val ivea_data_to_ivs : int32 ->
   int32 * int32 * int32 * int32 * int32 * int32 * int32
@@ -16,5 +36,3 @@ val ivea_data_to_ivs : int32 ->
 val ivs_to_ivea_data :
   int32 * int32 * int32 * int32 * int32 * int32 * int32
   -> int32
-
-val checksum_diff_for_aligned_int32 : int32 -> int32 -> int32
