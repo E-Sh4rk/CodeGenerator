8b64b1fa4a5e3c0eb1a17828e3ff1943f719bbbc
Mickael Laurent <esh4rk@gmail.com>
2021-02-26 23:36:36 +0100

    onlyRaw mode (only for testing)

--- ace_common.ml
+++ ace_common.ml
@@ -13,6 +13,14 @@ let treat_command fmt arm =
   code
 
 let main fmt (headers, program) exit =
+  let onlyraw =
+    match Parser_ast.get_header headers "onlyraw" with
+    | HNone -> false
+    | HBool b -> b
+    | _ -> failwith "Invalid headers."
+  in
+  if onlyraw && exit <> None
+  then failwith "Only-raw mode does not support exit codes." ;
   let start =
     match Parser_ast.get_header headers "start" with
     | HNone -> 0
@@ -34,21 +42,30 @@ let main fmt (headers, program) exit =
   let res = program |> 
     Optimizer.fix_arm |>
     List.map (treat_command fmt) in
-  try
-    let boxes_codes = Boxes.fit_codes_into_boxes ~fillers ~start ~exit res in
-    Format.fprintf fmt "@.%a@." Boxes.pp_boxes_names boxes_codes ;
-    let size = List.length boxes_codes in
-    begin
-      if size > Boxes.nb_boxes
-      then
-        Format.fprintf fmt "Warning: Not enough space... Need %n/%n boxes.@."
-        size Boxes.nb_boxes
-    end ;
-    begin
-      if List.exists (fun c -> Name.is_code_writable c |> not) boxes_codes
-      then Format.fprintf fmt "Warning: Contains unwritable characters...@."
-    end ;
-    Format.fprintf fmt "Raw data (in hexadecimal):@." ;
-    boxes_codes |> List.iter (Format.fprintf fmt "%a" Boxes.pp_box_raw)
-  with Exit.NoExitCode ->
-    Format.fprintf fmt "Error: The exit code overlaps this code (too long?).@."
+  if onlyraw
+  then begin
+    let start = List.init start (fun _ -> 0) in
+    let res = List.concat (start::res) in
+    Format.fprintf fmt "@.Raw data (in hexadecimal):@." ;
+    List.iter (Format.fprintf fmt "%02X @?") res ;
+    Format.fprintf fmt "@."
+  end else
+    try
+      let boxes_codes = Boxes.fit_codes_into_boxes ~fillers ~start ~exit res in
+      Format.fprintf fmt "@.%a@." Boxes.pp_boxes_names boxes_codes ;
+      let size = List.length boxes_codes in
+      begin
+        if size > Boxes.nb_boxes
+        then
+          Format.fprintf fmt "Warning: Not enough space... Need %n/%n boxes.@."
+          size Boxes.nb_boxes
+      end ;
+      begin
+        if List.exists (fun c -> Name.is_code_writable c |> not) boxes_codes
+        then Format.fprintf fmt "Warning: Contains unwritable characters...@."
+      end ;
+      Format.fprintf fmt "Raw data (in hexadecimal):@." ;
+      boxes_codes |> List.iter (Format.fprintf fmt "%a" Boxes.pp_box_raw) ;
+      Format.fprintf fmt "@."
+    with Exit.NoExitCode ->
+      Format.fprintf fmt "Error: The exit code overlaps this code (too long?).@."
