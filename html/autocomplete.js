271b84cfd7cc973194b042829dbcf5f80b941234
Mickael LAURENT <esh4rk@gmail.com>
2024-09-23 20:23:31 +0200

    add autocomplete feature

--- html/autocomplete.js
+++ html/autocomplete.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'fontSize',
         'fontWeight',
         'letterSpacing',
+        'textSizeAdjust',
         'lineHeight',
         'padding',
         'textDecoration',
